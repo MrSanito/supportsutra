149a729d4fd97f568288a573f76d083c45935812
unknown <1988krishnani@gmail.com>
2026-04-23 16:17:43 +0530

    updated cors

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -15,7 +15,7 @@ app.use(cookieParser());
 const port = process.env.PORT || 3001;
 
 app.use(cors({
-  origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
+  origin: ["http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:3001", "http://127.0.0.1:3001", "https://supportsutra.zynito.in", "https://www.supportsutra.zynito.in"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
